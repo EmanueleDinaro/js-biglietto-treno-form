d6cf69362a22f7c1a04ea4fd10fd7061e5addede
Emanuele Dinaro <ema.dinaro@gmail.com>
2024-12-11 01:50:19 +0100

    feat: add js draft

--- js/script.js
+++ js/script.js
@@ -1,3 +1,37 @@
-const fullNameFormElm = document.getElementById ('fullname')
-const kmFormElm = document.getElementById ('km')
-const ageFormElm = document.getElementById ('age')
+//FORM
+const formElm = document.getElementById('form');
+
+const fullNameFormElm = document.getElementById('fullname')
+const kmFormElm = document.getElementById('km')
+const ageFormElm = document.getElementById('age')
+
+const ticketNameElm = document.getElementById('ticket-name');
+const ticketTypeElm = document.getElementById('ticket-type');
+const carriageElm = document.getElementById('carriage');
+const cpCodeElm = document.getElementById('cp-code');
+const ticketPriceElm = document.getElementById('ticket-price');
+
+const ticketElm = document.querySelector('ticket-train')
+const hiddenTicketElm = document.querySelector('d-none')
+
+formElm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const ticketPrice = kmFormElm * 0.21;
+    let reductTicketprice
+    
+    if (ageFormElm === 'maggiorenne') {
+        reductTicketprice = ticketPrice - (ticketPrice * 20) / 100
+        console.log(reductTicketprice)
+    } else (ageFormElm === 'senior')
+        reductTicketprice = ticketPrice - (ticketPrice * 40) / 100
+        console.log(reductTicketprice)
+    
+
+    ticketNameElm.innerHTML = fullNameFormElm.value
+    ticketTypeElm.innerHTML = ageFormElm.value
+
+
+
+    // ticketElm.classList.remove(".d-none");
+})
